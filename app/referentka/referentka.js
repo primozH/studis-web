e110a6fea1324528de17cc4613f476b72cdaec6d
aliize <ap9387@student.uni-lj.si>
2018-03-31 00:18:19 +0200

    dodano iskanje po vpisni stevilki in zacetnicah imena/priimka

--- app/referentka/referentka.js
+++ app/referentka/referentka.js
@@ -32,24 +32,55 @@ angular.module('myApp.referentka', ['ngRoute'])
 .controller('ReferentkaCtrl', ['$scope', '$http', function($scope, $http) {
 	//to bo v resnici false na začetku, zdej sm dala na true sam zato da se vidi forma prikaza rezultatov
 	//uporablja se pr ng-show
-	$scope.prikazi_rezultat_iskanja = true;
+	$scope.prikazi_rezultat_iskanja = false;
 
 	$scope.isci_po_vpisni = function() {
-		/*$http.get('http://localhost:8080/api/v1/student/1').then(function(response) { 
-	      /*if (response.data.length > 0) {
-	      	//pokaze rezultate iskanja po vpisni stevilki
-	      	//nevem zdejle cisto tocno kaj dobim v responsu, mislm ktere podatke o studentu?, bom popravla
-	        $prikazi_rezultat_iskanja = true;
-	        $iskanje_ime = response.data.ime;
-	        $iskanje_priimek = response.data.priimek;
-	        $iskanja_vpisna = response.data.vpisna;
-	      }
-	      else {
-	      	$scope.status_iskanje = "ne najdem študenta";        
-	      }
-	    }*/
+    if ($scope.vpisna_isci.match(/^[0-9]+$/) === null) {
+      $scope.prikazi_rezultat_iskanja = false;
+      $scope.status_iskanje = "vpisna številka naj bo naravno število";
+      return;
+    }
+    if ($scope.vpisna_isci.length != 8) {
+      $scope.prikazi_rezultat_iskanja = false;
+      $scope.status_iskanje = "vpisna številka mora biti dolga 8 znakov";
+      return;
+    }
 
+    $http.get('http://localhost:8080/api/v1/student?filter=vpisnaStevilka:EQ:' + $scope.vpisna_isci).then(function(response) {
+      if (response.data.length > 0) {
+        $scope.status_iskanje = "";
+        $scope.prikazi_rezultat_iskanja = true;
+        $scope.studenti = response.data;
+      }
+      else {
+        $scope.prikazi_rezultat_iskanja = false;
+        $scope.status_iskanje = "ne najdem tega študenta";
+      }      
+
+    });
 	};
+
+  $scope.isci_po_zacetnicah = function() {
+    if (!$scope.zac1_isci.match(/[a-z]/i) || !$scope.zac2_isci.match(/[a-z]/i)) {
+      $scope.prikazi_rezultat_iskanja = false;
+      $scope.status_iskanje = "za začetnici imena in priimka prosim vnesi črki [a-zA-Z]";
+      return;
+    }
+
+    $http.get('http://localhost:8080/api/v1/student?filter=priimek:LIKEIC:'+$scope.zac2_isci+'%%20ime:LIKEIC:'+$scope.zac1_isci+'%'
+).then(function(response) {
+      if (response.data.length > 0) {
+        $scope.status_iskanje = "";
+        $scope.prikazi_rezultat_iskanja = true;
+        $scope.studenti = response.data;
+      }
+      else {
+        $scope.prikazi_rezultat_iskanja = false;
+        $scope.status_iskanje = "ne najdem tega študenta";
+      }      
+
+    });
+  }
 
 
 	$scope.showContent = function($fileContent){
