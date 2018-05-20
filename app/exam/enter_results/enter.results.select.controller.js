72696d22f7379080473a7520eadbb325ed2cc43b
luka <luka1.zorko@gmail.com>
2018-05-20 15:06:16 +0200

    fixes

--- app/exam/enter_results/enter.results.select.controller.js
+++ app/exam/enter_results/enter.results.select.controller.js
@@ -6,8 +6,9 @@
         var vm = this;
 
         vm.subjectSelected = false;
+        vm.yearSelected = false;
 
-        examService.getAllSubjects()
+        /*examService.getAllSubjects()
             .then(
                 function success(response){
                     vm.subjects = response.data;
@@ -17,13 +18,9 @@
                 function error(error){
                     console.log(error);
                 }
-            );
+            );*/
 
-
-        $('.selectpicker').selectpicker();
-
-
-        vm.getExamsForSubject = function(){
+        /*vm.getExamsForSubject = function(){
             console.log(vm.predmet);
             examService.getExamsForSubject(vm.predmet)
                 .then(
@@ -36,13 +33,53 @@
                         console.log(error);
                     }
                 )
+        };*/
+
+
+        vm.getExamsForSubject = function(){
+            vm.exams = [];
+            examService.getExamsForSubjectYear(vm.izvajanjePredmeta.predmet.sifra, vm.studijskoLeto)
+                .then(
+                    function success(response){
+                        console.log(response);
+                        if(response.status !== 204){
+                            vm.exams = vm.exams.concat(response.data);
+                        }
+                        vm.nosilci = [vm.izvajanjePredmeta.nosilec1, vm.izvajanjePredmeta.nosilec2, vm.izvajanjePredmeta.nosilec3];
+                        vm.subjectSelected = true;
+                    },
+                    function error(error){
+                        console.log(error);
+                    }
+                )
+        };
+
+        vm.getSubjectsForYear = function(){
+            examService.getAllSubjects(vm.studijskoLeto)
+                .then(
+                    function success(response){
+                        vm.subjects = response.data;
+                        console.log("subjects");
+                        console.log(vm.subjects);
+                        vm.yearSelected = true;
+                    },
+                    function error(error){
+                        console.log(error);
+                    }
+                );
         };
 
         vm.enterResults = function(x){
             console.log("X");
             console.log(x);
             examService.setData(x);
-            $location.path("/vnosRezultatov/" + vm.predmet);
+            $location.path("/vnosRezultatov/" + x.id);
+        };
+
+        vm.reset = function(yearReset){
+            if(yearReset)
+                vm.yearSelected = false;
+            vm.subjectSelected = false;
         };
     }
 
